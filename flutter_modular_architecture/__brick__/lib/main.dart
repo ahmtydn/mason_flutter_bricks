70be3ec6a41059b50ff0c22bf6c6dd149993e9b4
Ahmet Aydın <84200491+ahmtydn@users.noreply.github.com>
2026-01-20 09:39:53 +0300

    refactor: update import path for device_preview_tools.dart by removing the 'extensions' subdirectory.

--- flutter_modular_architecture/__brick__/lib/main.dart
+++ flutter_modular_architecture/__brick__/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:device_preview_plus/device_preview_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:{{project_name.snakeCase()}}/product/init/app.dart';
 import 'package:{{project_name.snakeCase()}}/product/init/bootstrap.dart';
-import 'package:{{project_name.snakeCase()}}/product/utility/extensions/device_preview_tools.dart';
+import 'package:{{project_name.snakeCase()}}/product/utility/device_preview_tools.dart';
 
 Future<void> main() async {
   await ProductBootstrapper.initialize();
